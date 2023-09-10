e862b8540688d756ef360156869b2a318feb5f18
Rod-rig <cherniakvadim08@gmail.com>
2023-09-10 22:46:26 +0300

    fix fetch url

--- server/fetch-matches.js
+++ server/fetch-matches.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export const fetchMatches = async () => {
-  return fetch(`${process.env.ROOT_URL}:${process.env.PORT}/matches`)
+  return fetch(`${process.env.ROOT_URL}/matches`)
     .then((response) => response.json())
     .catch((error) => {
       console.log("Couldn't fetch matches: ", error);
