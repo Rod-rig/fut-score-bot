e026b52928cafb12039301e4ec794f18a8e2b554
Rod-rig <cherniakvadim08@gmail.com>
2024-04-09 21:44:32 +0300

    Add cors

--- site/server/main.js
+++ site/server/main.js
@@ -1,11 +1,11 @@
+import "dotenv/config";
 import express from "express";
 import ViteExpress from "vite-express";
+import cors from "cors";
 
 const app = express();
-
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+app.use(cors());
+ViteExpress.config({ mode: process.env.NODE_ENV });
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
