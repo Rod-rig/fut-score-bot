4fdc786dda7b502cc6d804cba0b0acc418acb81c
Rod-rig <cherniakvadim08@gmail.com>
2024-09-17 22:22:26 +0300

    fix 1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static(__dirname + "/dist"));
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
 app.use(express.json());
 app.use("/api", userRouter);
