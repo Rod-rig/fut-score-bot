df88458bb46772a81c4d8b7e0da08c24f449dbba
Rod-rig <cherniakvadim08@gmail.com>
2024-09-17 22:57:43 +0300

    fix 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import "dotenv/config";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import "dotenv/config";
 import { userRouter } from "./routes/user.js";
 import { eventRouter } from "./routes/event.js";
 import { oddRouter } from "./routes/odd.js";
@@ -9,18 +7,13 @@ import { predictionRouter } from "./routes/prediction.js";
 import { resultsRouter } from "./routes/results.js";
 import { startBot } from "./bot/index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
-app.use(express.static(__dirname + "../dist"));
+app.use(express.static("dist"));
 app.use(express.json());
-app.get("/api", userRouter);
-app.get("/api", eventRouter);
-app.get("/api", oddRouter);
-app.get("/api", predictionRouter);
-app.get("/api", resultsRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
-});
+app.use("/api", userRouter);
+app.use("/api", eventRouter);
+app.use("/api", oddRouter);
+app.use("/api", predictionRouter);
+app.use("/api", resultsRouter);
 app.listen(process.env.PORT);
 startBot();
