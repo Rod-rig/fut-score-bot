7007c7b0799c54d596e5c46fca364efd1c13eedf
Rod-rig <cherniakvadim08@gmail.com>
2024-09-19 21:54:39 +0300

    fix 13

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import path from "path";
 import express from "express";
-import { fileURLToPath } from "url";
 import "dotenv/config";
 import { userRouter } from "./routes/user.js";
 import { eventRouter } from "./routes/event.js";
@@ -9,23 +7,13 @@ import { predictionRouter } from "./routes/prediction.js";
 import { resultsRouter } from "./routes/results.js";
 import { startBot } from "./bot/index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
-console.log("__dirname", __dirname);
-console.log(
-  "index.html location",
-  path.resolve(__dirname, "../../dist", "index.html"),
-);
-app.use(express.static(__dirname + "../../dist"));
+app.use(express.static("dist"));
 app.use(express.json());
 app.use("/api", userRouter);
 app.use("/api", eventRouter);
 app.use("/api", oddRouter);
 app.use("/api", predictionRouter);
 app.use("/api", resultsRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../dist", "index.html"));
-});
 app.listen(process.env.PORT);
 startBot();
