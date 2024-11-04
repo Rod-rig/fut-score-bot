512cf34edaed669064b35df4a0f26aa620eacd12
Rod-rig <cherniakvadim08@gmail.com>
2024-11-04 19:17:34 +0200

    remove auto updating of prediction of Average

--- src/api/create-prediction.js
+++ src/api/create-prediction.js
@@ -6,7 +6,6 @@ export const createPrediction = async (value, userId, eventId) => {
     let prediction = await getPrediction(userId, eventId);
     const event = await fetchMatchById(eventId);
     if (event.status === "FINISHED") return;
-    await updateAveragePrediction(value, eventId);
     if (prediction) {
       await updatePrediction(value, userId, eventId);
     } else {
@@ -71,56 +70,6 @@ const updatePrediction = async (value, userId, eventId) => {
         eventId,
       }),
     });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const calculateAverageScore = (scores) => {
-  const sum = scores.reduce(
-    (acc, curr) => {
-      const [home, away] = curr.split(":");
-      return {
-        home: acc["home"] + parseInt(home),
-        away: acc["away"] + parseInt(away),
-      };
-    },
-    {
-      home: 0,
-      away: 0,
-    },
-  );
-  const homeAverage = Math.round(sum["home"] / scores.length);
-  const awayAverage = Math.round(sum["away"] / scores.length);
-  return `${homeAverage}:${awayAverage}`;
-};
-
-const getEventWithPredictions = async (eventId) => {
-  try {
-    const response = await fetch(
-      `${process.env.ROOT_URL}/api/event-predictions/${eventId}`,
-    );
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const updateAveragePrediction = async (value, eventId) => {
-  try {
-    const userId = 100000002;
-    const prediction = await getPrediction(userId, eventId);
-    if (prediction) {
-      const event = await getEventWithPredictions(eventId);
-      const eventScores = event.predictions
-        .filter((p) => p.userId !== userId || p.value !== "Any Other")
-        .map((p) => p.value);
-      const scores = [...eventScores, value];
-      const newValue = calculateAverageScore(scores);
-      await updatePrediction(newValue, userId, eventId);
-    } else {
-      await createNewPrediction(value, userId, eventId);
-    }
   } catch (error) {
     console.log(error);
   }
